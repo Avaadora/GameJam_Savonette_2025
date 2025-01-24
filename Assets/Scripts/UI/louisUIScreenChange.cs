c2de2e57d5cd5551af4f89ffad534ae41b63c25d
Thunder4242 <louisdutheil@free.fr>
2025-01-24 23:07:16 +0100

    Main & Pause menus
    La majorité des fonctions du main & pause menus

--- Assets/Scripts/UI/louisUIScreenChange.cs
+++ Assets/Scripts/UI/louisUIScreenChange.cs
@@ -7,4 +7,8 @@ public class louisUIScreenChange : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }

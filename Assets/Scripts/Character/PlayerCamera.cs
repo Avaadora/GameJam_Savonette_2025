068b39831387cc483bcef343a785782526e01142
nagaboyy <nagaboyy@gmail.com>
2025-01-25 06:58:25 +0100

    Controller OK + Cam OK + Props OK

--- Assets/Scripts/Character/PlayerCamera.cs
+++ Assets/Scripts/Character/PlayerCamera.cs
@@ -2,27 +2,58 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    [SerializeField] private Transform _orientation;
-    [SerializeField] private Transform _player;
-    [SerializeField] private float _rotationSpeed;
+    [Header("--- Camera Settings ---")]
+    [SerializeField] private float rotationSpeed; // vitesse de rotation de la cam
+    [SerializeField] private CameraStyle currentStyle; // type de caméra libre ou fixe
+    public enum CameraStyle
+    {
+        Freeview,
+        Locked
+    }
+
+    [Header("References")]
+    // player
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform playerMesh;
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private Transform playerOrientation;
+    [SerializeField] private Transform lookAt;
+
+    // cameras
+    [SerializeField] private GameObject freeviewCamera;
+    [SerializeField] private GameObject lockedCamera;
+
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // lock le curseur au centre de l'écran
-        Cursor.visible = false; // cache le curseur
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    private void LateUpdate()
+    private void Update()
     {
-        Vector3 viewDir = _player.position - new Vector3(transform.position.x, _player.position.y, transform.position.z);
-        _orientation.forward = viewDir.normalized;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = _orientation.forward * verticalInput + _orientation.right * horizontalInput;
+        // change l'orientation du player
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        playerOrientation.forward = viewDir.normalized;
 
-        if (inputDir != Vector3.zero)
+        // change la rotation du player
+        if(currentStyle == CameraStyle.Freeview)
         {
-            _player.forward = Vector3.Slerp(_player.forward, inputDir.normalized, _rotationSpeed * Time.deltaTime);
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = playerOrientation.forward * verticalInput + playerOrientation.right * horizontalInput;
+
+            if (inputDir != Vector3.zero)
+            {
+                playerMesh.forward = Vector3.Slerp(playerMesh.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+        }
+        else if(currentStyle == CameraStyle.Locked)
+        {
+            Vector3 dirToCombatLookAt = lookAt.position - new Vector3(transform.position.x, lookAt.position.y, transform.position.z);
+            playerOrientation.forward = dirToCombatLookAt.normalized;
+
+            playerMesh.forward = dirToCombatLookAt.normalized;
         }
     }
 }
--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -2,83 +2,98 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player Components")]
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private Transform _orientation;
+    [Header("--- Player Settings ---")]
+    [SerializeField] private float moveSpeed; // vitesse de mouvement
+    [SerializeField] private float groundDrag; // friction avec le sol
+    [SerializeField] private float jumpForce; // force de jump
+    [SerializeField] private float jumpCooldown; // cooldown avant de pouvoir de rejump
+    [SerializeField] private float airMultiplier; // air control
 
-    [Header("Player Move")]
-    [SerializeField][Range(0.0f, 200.0f)] private float _speed = 50.0f;
-    private Vector3 _moveDirection;
-    private float _horizontalInput = 0.0f; // input x (Q / D)
-    private float _verticalInput = 0.0f; // input y (Z / S)
+    [Header("Références")]
+    [SerializeField] private Transform playerOrientation;
+    [SerializeField] private float playerHeight; // taille du joueur (mettre la valeur de scale y)
+    [SerializeField] private LayerMask groundLayer; // layer du sol
+    private bool canJump = true;
+    private bool isGrounded = false;
 
-    [Header("Player Jump")]
-    [SerializeField] private float _jumpForce = 1.0f;
-    [SerializeField] private float _jumpCooldown = 1.0f;
-    [SerializeField] private float _airMultiplier = 1.0f;
-    [SerializeField] private LayerMask _groundLayer;
-    private bool _canJump = true;
-    private bool _isGrounded = false;
-
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 moveDirection;
+    private Rigidbody playerRigidbody;
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        // _rigidbody.freezeRotation = true;
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody.freezeRotation = true;
     }
 
+    private void Update()
+    {
+        // ground check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, groundLayer);
+
+        Inputs();
+        SpeedControl();
+
+        // change la friction entre sol et air
+        playerRigidbody.linearDamping = isGrounded ? groundDrag : 0f;
+    }
 
     private void FixedUpdate()
     {
-        _horizontalInput = Input.GetAxis("Horizontal"); // get input x (-1 to 1)
-        _verticalInput = Input.GetAxis("Vertical"); // get input y (-1 to 1)
-        _isGrounded = Physics.Raycast(transform.position - new Vector3(0.0f, transform.localScale.y / 2.0f, 0.0f), Vector3.down, 0.1f, _groundLayer); // check si player au sol (appliquer layer "ground" sur les objets où il peut sauter)
-
-        PlayerMove();
-        PlayerJump();
-
-        // application des forces
-        _rigidbody.AddForce(_moveDirection.normalized * _speed, ForceMode.Force);
+        Move();
     }
 
-    private void PlayerMove() // gère les mouvements du player
+    private void Inputs()
     {
-        _moveDirection = _orientation.forward * _verticalInput + _orientation.right * _horizontalInput; // get direction du player
+        // inputs de move
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        // gère la vélocité
-        Vector3 flatVel = new (_rigidbody.linearVelocity.x, 0f, _rigidbody.linearVelocity.z);
-        if (flatVel.magnitude > _speed)
+        // conditions pour sauter
+        if(Input.GetButton("Jump") && canJump && isGrounded)
         {
-            Vector3 limitedVel = flatVel.normalized * _speed;
-            _rigidbody.linearVelocity = new (limitedVel.x, _rigidbody.linearVelocity.y, limitedVel.z);
+            canJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
-        _rigidbody.linearDamping = _isGrounded ? 5.0f : 0.0f;
     }
 
-    private void PlayerJump() // gère le saut du player
+    private void Move()
     {
-        if (!_isGrounded) // si player dans les airs
-        {
-            _speed *= _airMultiplier; // modifier air control
-            _moveDirection += (_moveDirection.y < 0 ? Physics.gravity * 3 : Physics.gravity) * Time.deltaTime; // retomber plus vite
-        }
+        // calcule la direction du mouvement
+        moveDirection = playerOrientation.forward * verticalInput + playerOrientation.right * horizontalInput;
 
-        if (Input.GetButton("Jump") && _isGrounded && _canJump)
+        // gestion du air control
+        if(isGrounded) // au sol
         {
-            _canJump = false;
-            _rigidbody.linearVelocity = new Vector3(_rigidbody.linearVelocity.x, 0f, _rigidbody.linearVelocity.z);
-            _rigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
-            Invoke(nameof(ResetJump), _jumpCooldown);
+            playerRigidbody.AddForce(10f * moveSpeed * moveDirection.normalized, ForceMode.Force);
         }
+        else if(!isGrounded) // dans les air
+        {
+            playerRigidbody.AddForce(10f * airMultiplier * moveSpeed * moveDirection.normalized, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new(playerRigidbody.linearVelocity.x, 0f, playerRigidbody.linearVelocity.z);
+
+        // limite la vélocité
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            playerRigidbody.linearVelocity = new Vector3(limitedVel.x, playerRigidbody.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0f, playerRigidbody.linearVelocity.z);
+        playerRigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
     private void ResetJump()
     {
-        _canJump = true;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position - new Vector3(0.0f, transform.localScale.y / 2.0f, 0.0f), transform.position - new Vector3(0.0f, transform.GetComponent<BoxCollider>().size.y / 2.0f - 0.1f, 0.0f));
+        canJump = true;
     }
 }

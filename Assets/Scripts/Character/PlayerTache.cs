3cf808e8165a5f70a3704708a222802b11267137
game2 <game2@ectls.local>
2025-01-25 21:06:20 +0100

    Decals needs debug

--- Assets/Scripts/Character/PlayerTache.cs
+++ Assets/Scripts/Character/PlayerTache.cs
@@ -5,24 +5,26 @@ using UnityEngine.Rendering.Universal;
 public class PlayerTache : MonoBehaviour
 {
     [SerializeField] private GameObject myPrefab;
+    private IEnumerator coroutine;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+                coroutine = WaitIAmInTheBathroom(2.0f);
+        StartCoroutine(coroutine);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private IEnumerator WaitIAmInTheBathroom()
+    private IEnumerator WaitIAmInTheBathroom(float waitTime)
     {
         var instance = Instantiate(myPrefab);
         var instancePlaneObject = instance.GetComponent<DecalProjector>();           //Il faut trouver autre chose que NetworkObject
         Instantiate(instancePlaneObject);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(waitTime);
     }
 }

cfa2b194cc44d70a5dfd0094e974350da1b15826
Ferrere-Julien <123733042+Ferrere-Julien@users.noreply.github.com>
2025-01-25 16:27:40 +0100

    VertexPaintEnCours

---
+++ Assets/Scripts/Character/VertexPainting.cs
@@ -0,0 +1,42 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class VertexPainting : MonoBehaviour
+{
+    private PlayerController myPlayerController;
+    private GameObject myObjet;
+    //public void SetColors(Color[] inColors);
+    private VertexPainting myVertex;
+    private BoxCollider myBoxCollider;
+    private Mesh myMesh;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //myVertex.OnCollisionEnter();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (myObjet.CompareTag("Player"))
+        {
+            //myMesh = GetComponent<MeshFilter>().mesh;
+            //myPlayerController.Update();
+            for (int i = 0; i < myMesh.vertexCount; i++)
+            {
+                new myMesh.vertexCount;
+                myMesh.GetVertexAttribute<VertexPainting>();
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
